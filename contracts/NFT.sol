9e31dcaa20e0f73f8c0f06abbf5989d940fd295f
Matt <matt@MSI.localdomain>
2023-09-14 03:10:03 -0400

    Create NFT

---
+++ contracts/NFT.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./ERC721Enumerable.sol";
+import "./Ownable.sol";
+
+contract NFT is ERC721Enumerable, Ownable {
+    uint256 public cost;
+    uint256 public maxSupply;
+    uint256 public allowMintingOn;
+    string public baseURI;
+
+    constructor(
+        string memory _name, 
+        string memory _symbol,
+        uint256 _cost,
+        uint256 _maxSupply,
+        uint256 _allowMintingOn,
+        string memory _baseURI
+    ) ERC721(_name, _symbol) {
+        cost = _cost;
+        maxSupply = _maxSupply;
+        allowMintingOn = _allowMintingOn;
+        baseURI = _baseURI;
+    }  
+
+
+
+}
---
+++ test/NFT.js
@@ -0,0 +1,65 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+
+describe('NFT', () => {
+  const NAME = 'Dapp Punks'
+  const SYMBOL = 'DP'
+  const COST = ether(10)
+  const MAX_SUPPLY = 25
+  const BASE_URI = 'ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/'
+
+  let nft,
+      deployer,
+      minter
+
+  beforeEach(async () => {
+    let accounts = await ethers.getSigners()
+    deployer = accounts[0]
+    minter = accounts[1]
+  }) 
+
+  describe('Deployment', () => {
+    const ALLOW_MINTING_ON = (Date.now() + 120000).toString().slice(0, 10) // 2 minutes from now
+
+    beforeEach(async () => {
+      const NFT = await ethers.getContractFactory('NFT')
+      nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
+    })
+
+    it('has correct name', async () => {
+      expect(await nft.name()).to.equal(NAME)
+    })
+
+    it('has correct symbol', async () => {
+      expect(await nft.symbol()).to.equal(SYMBOL)
+    })
+
+    it('returns the cost to mint', async () => {
+      expect(await nft.cost()).to.equal(COST)
+    })
+
+    it('returns the maximum total supply', async () => {
+      expect(await nft.maxSupply()).to.equal(MAX_SUPPLY)
+    })
+
+    it('returns the allowed minting time', async () => {
+      expect(await nft.allowMintingOn()).to.equal(ALLOW_MINTING_ON)
+    })
+
+    it('returns the base URI', async () => {
+      expect(await nft.baseURI()).to.equal(BASE_URI)
+    })
+
+    it('returns the owner', async () => {
+      expect(await nft.owner()).to.equal(deployer.address)
+    })
+
+  })
+
+})
