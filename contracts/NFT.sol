97fb63359a4ca8b7c89e2b2eb033958988b32305
Matt <matt@MSI.localdomain>
2023-09-19 02:34:19 -0400

    Mint NFTs

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,10 +5,15 @@ import "./ERC721Enumerable.sol";
 import "./Ownable.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+    string public baseURI;
+    string public baseExtension = ".json";
     uint256 public cost;
     uint256 public maxSupply;
     uint256 public allowMintingOn;
-    string public baseURI;
+
+    event Mint(uint256 amount, address minter);
 
     constructor(
         string memory _name, 
@@ -24,6 +29,49 @@ contract NFT is ERC721Enumerable, Ownable {
         baseURI = _baseURI;
     }  
 
+    function mint(uint256 _mintAmount) public payable {
+        // Only allow minting after specified time
+        require(block.timestamp >= allowMintingOn);
+        // Must mint at least 1 token
+        require(_mintAmount > 0); 
+        // Require enough payment
+        require(msg.value >= cost * _mintAmount);
+        
+        uint256 supply = totalSupply(); 
 
+        // Do not let them mint more tokens than available
+        require(supply + _mintAmount <= maxSupply); 
+
+        // Create a token
+        for(uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+        
+        // Emit event
+        emit Mint(_mintAmount, msg.sender);
+    }
+
+    // Return metadata IPFS url
+    // E.G. 'ipfs://Qmf7p6WszsHA7jsdXaDWkggdxBwCDQ5XUgXzwQjpdHFoHy/1.json'
+    function tokenURI(uint256 _tokenId) 
+        public 
+        view 
+        virtual 
+        override 
+        returns(string memory)
+    {
+        require(_exists(_tokenId), 'token does not exist');
+        return(string(abi.encodePacked(baseURI, _tokenId.toString(), baseExtension)));
+
+    }
+
+    function walletOfOwner(address _owner) public view returns(uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for(uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
+    }
 
 }
